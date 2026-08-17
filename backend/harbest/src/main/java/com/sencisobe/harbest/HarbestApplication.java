6c2ef2acad73fb7ac3fc546a1033773a2e42bc06
sencisobe <simonenciso1@gmail.com>
2026-08-17 11:29:11 +0200

    Creation of Spring package

---
+++ backend/harbest/src/main/java/com/sencisobe/harbest/HarbestApplication.java
@@ -0,0 +1,13 @@
+package com.sencisobe.harbest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarbestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarbestApplication.class, args);
+	}
+
+}
---
+++ backend/harbest/src/test/java/com/sencisobe/harbest/HarbestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sencisobe.harbest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarbestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
