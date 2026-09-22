ce3ed734102b15b2d7f23bf0f32ce8e312d3d1c3
sencisobe <simonenciso1@gmail.com>
2026-09-22 13:57:27 +0200

    New case studies Create Habit and GetWeeklyProgess

---
+++ backend/harbest/src/main/java/com/sencisobe/harbest/application/usecase/CreateHabit.java
@@ -0,0 +1,22 @@
+// application/usecase/CreateHabit.java
+package com.sencisobe.harbest.application.usecase;
+
+import com.sencisobe.harbest.domain.model.Habit;
+//import com.sencisobe.harbest.domain.repository.HabitRepository;
+
+
+public class CreateHabit {
+/* 
+    private final HabitRepository habitRepository;
+
+    public CreateHabit(HabitRepository habitRepository) {
+        this.habitRepository = habitRepository;
+    }
+
+    public Habit execute(String name, int dailyObjectiveTime) {
+        Habit habit = new Habit(name, dailyObjectiveTime);
+        habitRepository.save(habit);
+        return habit;
+    }
+    */
+}
---
+++ backend/harbest/src/main/java/com/sencisobe/harbest/application/usecase/GetWeeklyProgress.java
@@ -0,0 +1,5 @@
+package com.sencisobe.harbest.application.usecase;
+
+public class GetWeeklyProgress {
+
+}
